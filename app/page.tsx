988c5af1f1a0c1e13ab075c7f7487d4ab465966c
keiramo <keiramo@softcat.com>
2025-10-20 19:54:43 +0100

    redesigned home page - create and join is one button, room page created with clickable number cards and can copy room link to clipboard

--- app/page.tsx
+++ app/page.tsx
@@ -1,51 +1,51 @@
 /**
- * This is the start/login page of the application.
+ * This is the home/login page of the application.
  */
 "use client";
+
 import { useState } from "react";
+import { useRouter } from "next/navigation";
 import Image from "next/image";
 
 
 // Main Home Page Component
 export default function Home() {
-   const [roomId, setRoomId] = useState<string | null>(null);
+  const router = useRouter();
+  const [loading, setLoading] = useState<boolean>(false);
 
-   function generateRoomID(): string {
-
-    console.log("Generate button clicked: Generating Room ID");
+  /**
+   * Generates a random Room ID.
+   * @return void
+   */
+  function createAndJoinRoom(): void {
+    setLoading(true);
+    console.log("Generate button clicked");
     const newId = Math.random().toString(36).substring(2, 8).toUpperCase();
-    setRoomId(newId);
-    return newId;
-  }
-
-  function copyToClipboard() {
-    console.log("Copy button clicked");
+    console.log("Generating room", newId);
+    router.push(`/room?roomId=${newId}`); // Navigate to the room page with the new Room ID
   }
 
    return (
     <main className="min-h-screen flex flex-col items-center justify-center p-8 text-center">
-      <div className="bg-purple-800 border-4 border-purple-950 rounded-xl px-4 py-4 shadow-md flex items-center justify-center mb-8">
+      <div className="border-4 border-purple-950 rounded-xl px-4 py-4 shadow-md flex items-center justify-center mb-8">
         <h1 className="text-4xl font-bold text-white">You Better Vote!</h1>
       </div>
 
       <div className="flex flex-col items-center gap-4 border-2 border-gray-300 rounded-xl p-6 bg-white shadow-md">
         <button
           className="bg-blue-600 text-white rounded-lg px-6 py-3 hover:bg-blue-700 transition"
-          onClick={generateRoomID}
+          onClick={createAndJoinRoom}
+          disabled={loading}
         >
-          Generate Room ID
-        </button>
-        <p className="text-lg font-medium text-gray-900">
-          Room ID: <span className="font-mono bg-gray-200 text-gray-900 px-2 py-1 rounded">{roomId}</span>
-          <button onClick={copyToClipboard} className="hover:opacity-70 px-2 py-1">
-            <Image
-              src="/copy.svg"
-              alt="Copy room ID"
+          {loading ? "Creating Room..." : "Create & Join Room"}
+          <Image
+              src="/log-in.svg"
+              alt="log-in arrow"
               width={20}
               height={20}
+              className="inline-block ml-2 align-middle filter invert"
             />
-          </button>
-        </p>
+        </button>
       </div>
 
       <footer className="mt-12 text-gray-500 text-sm">
